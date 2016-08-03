c871fe817c12ec024301bf3f1531589f92ebc570
OscarIuliano <oiuliano90@gmail.com>
2016-08-03 17:34:41 +0200

    SendMail

--- new tem/Studio Luciani Energia srl/theme/contact.php
+++ new tem/Studio Luciani Energia srl/theme/contact.php
@@ -59,7 +59,7 @@ function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= 'From: ' . $from . "\r\n";
-	
+	console.log('TEst');
 	$result = mail($to,$subject,$message,$headers);
 	
 	if ($result) return 1;
